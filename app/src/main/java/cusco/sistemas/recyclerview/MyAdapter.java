a37f3da9e2c17519e44faf9ece303d2f72a8bf2f
Fernando <cuzcator@gmail.com>
2019-09-04 17:32:50 -0500

    RecyclerView
    Parte 2

--- app/src/main/java/cusco/sistemas/recyclerview/MyAdapter.java
+++ app/src/main/java/cusco/sistemas/recyclerview/MyAdapter.java
@@ -1,5 +1,6 @@
 package cusco.sistemas.recyclerview;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -7,23 +8,37 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private List<String> names;
+    private int layout;
+    private OnItemClickListener itemClickListener;
+
+    public MyAdapter(List<String> names, int layout, OnItemClickListener listener){
+        this.names = names;
+        this.layout = layout;
+        this.itemClickListener = listener;
+    }
 
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext()).inflate(this.layout, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        holder.bind(this.names.get(position), itemClickListener);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return this.names.size();
     }
 
 
@@ -34,6 +49,20 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
             super(view);
             this.name = (TextView) view.findViewById(R.id.textViewName);
         }
+
+        public void bind(final String name, final OnItemClickListener listener){
+            this.name.setText(name);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listener.onItemClick(name, getAdapterPosition());
+                }
+            });
+        }
+    }
+
+    public interface OnItemClickListener{
+        void onItemClick(String name, int pos);
     }
 }
 
